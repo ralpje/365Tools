ab02e6c062de0f6e17e0996b10447c4753c99d4a
Ralph Eckhard <Ralph Eckhard>
2017-02-16 12:06:26 +0100

    Added Open-MSOLConnection function

---
+++ Open-MSOLConnection.ps1
@@ -0,0 +1,15 @@
+function Open-MSOLConnection
+{
+  <#
+    .SYNOPSIS
+    Connects and loads a MSOL PoSH Connection.
+    .DESCRIPTION
+    Gets credential for MSOL Session, configures a new session and loads it.
+    .EXAMPLE
+    Open-MSOLConnection
+    #>
+  $LiveCred = Get-Credential
+  $Session = New-PSSession -Name MSOL -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/PowerShell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+  Import-PSSession $Session
+  connect-msolservice -credential $LiveCred
+}
