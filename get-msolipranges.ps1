a6e3f910cdd84aa1eb5343d04550c890a6905786
powershellpr0mpt <robert.prust@gmail.com>
2017-09-14 21:44:50 +0200

    changed to advanced function, added IPType parameter, cleaned up $Products array and removed IPv4 name from IP range naming.

--- get-msolipranges.ps1
+++ get-msolipranges.ps1
@@ -4,23 +4,31 @@ function Get-MSOLIPRanges {
     Finds IP range for given O365 Products to create firewall rules.
     .DESCRIPTION
     Finds IP range for given O365 Products to create firewall rules.
+    .PARAMETER IPType
+    Select which IP type you want to find the ranges for.
+    Possible options are IPv4 and IPv6, default is IPv4.
     .EXAMPLE
-    get-msolipranges
+    Get-MSOLIPRanges
     #>
+    [cmdletbinding()]
+    param(
+        [Parameter(Position = 0)]
+        [ValidateSet('IPv4', 'IPv6')]
+        [string]$IPType = 'IPv4'
+    )
     $Office365IPsXml = New-Object System.Xml.XmlDocument
     $Office365IPsXml.Load("https://support.content.office.net/en-us/static/O365IPAddresses.xml")
     $updated = $Office365IPsXml.products.updated
     Write-Output "Last updated: $updated"
-    [array]$products = $Office365IPsXml.products.product | Select-Object Name | ogv -PassThru
-    foreach ($Product in ($Office365IPsXml.products.product | Where-Object ( {$Products -match $_.Name}) | Sort-Object Name)) {
-        $IPv4Ranges = $Product | Select-Object -ExpandProperty Addresslist | Where-Object {$_.Type -eq "IPv4"}
-        $IPv4Ranges = $IPv4Ranges | Where-Object {$_.address -ne $null} | Select-Object -ExpandProperty address
-        foreach ($Range in $IPv4Ranges) {
-            $ProductIPv4Range = New-Object -TypeName psobject -Property @{
-                'Product'   = $Product.Name;
-                'IPv4Range' = $Range;
+    [array]$Products = $Office365IPsXml.products.product | Sort-Object Name | Out-GridView -PassThru -Title 'Select Product(s)'
+    foreach ($Product in $Products) {
+        $IPRanges = ($Product | Select-Object -ExpandProperty Addresslist | Where-Object {$_.Type -eq $IPType}) | Where-Object {$_.address -ne $null} | Select-Object -ExpandProperty address
+        foreach ($Range in $IPRanges) {
+            $ProductIPRange = New-Object -TypeName PSObject -Property @{
+                'Product' = $Product.Name;
+                'IPRange' = $Range;
             }
-            Write-Output $ProductIPv4Range | Select-Object Product, IPv4Range
+            Write-Output $ProductIPRange | Select-Object Product, IPRange
         }
     }
 }
